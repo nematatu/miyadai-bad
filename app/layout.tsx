47f5ef16f84485ffc4fc9797e0b4b65f06de4c65
nematatu <nematatu1003@gmail.com>
2024-10-13 20:24:37 +0900

    update:img→Image

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,10 @@ import Footer from '@/components/Footer';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
+	openGraph: {
+		title: '宮崎大学 バドミントン部',
+		description: '宮崎大学バドミントン部の公式ホームページです。',
+  },
   title: '宮崎大学 バドミントン部',
   icons:"data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🏸</text></svg>",
   description: '宮崎大学バドミントン部の公式ホームページです。',
