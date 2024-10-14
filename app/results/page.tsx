7a91503ac0008b3978a9975a441de4c0b68574ab
nematatu <nematatu1003@gmail.com>
2024-10-14 17:16:48 +0900

    update:img→Image

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,10 +1,31 @@
 import Link from "next/link";
 
-const domain = 'results';
+// マイクロCMSからデータを取得する関数
+const getList = async (domain: string) => {
+  const endpoint = `https://miyadai-bad.microcms.io/api/v1/${domain}`;
+  const response = await fetch(endpoint, {
+    method: 'GET',
+    headers: {
+      'X-MICROCMS-API-KEY': process.env.MICROCMS_API_KEY || '',
+      'Content-Type': 'application/json',
+    },
+    cache: 'no-store',
+  });
 
-// ここでデータを取得
+  if (!response.ok) {
+    const errorData = await response.text(); // エラー詳細を取得
+    console.error(`Error fetching data: ${response.status} ${errorData}`);
+    throw new Error(`Failed to fetch data: ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data;
+};
+
+// ページコンポーネント
 export default async function StaticPage() {
-  const { contents } = await getList(domain);
+  const domain = 'results';
+  const { contents } = await getList(domain); // データを取得
 
   if (!contents || contents.length === 0) {
     return <h1>No contents</h1>;
@@ -25,25 +46,3 @@ export default async function StaticPage() {
     </div>
   );
 }
-
-// データを取得する関数
-export const getList = async (domain: string) => {
-  const endpoint = `https://miyadai-bad.microcms.io/api/v1/${domain}`;
-  const response = await fetch(endpoint, {
-    method: 'GET',
-    headers: {
-      'X-MICROCMS-API-KEY': process.env.MICROCMS_API_KEY || '',
-      'Content-Type': 'application/json',
-    },
-    cache: 'no-store',
-  });
-
-  if (!response.ok) {
-    const errorData = await response.text(); // エラー詳細を取得
-    console.error(`Error fetching data: ${response.status} ${errorData}`);
-    throw new Error(`Failed to fetch data: ${response.status}`);
-  }
-
-  const data = await response.json();
-  return data;
-};
