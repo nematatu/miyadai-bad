e5daa63eed19968bb95f22419971bae103a140ac
nematatu <nematatu1003@gmail.com>
2024-10-14 19:36:50 +0900

    update:img→Image

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import Carousel from '@/components/Carousel';
 import Link from 'next/link';
-// import { getAllLists } from '@/libs/microcms';
+import { getAllLists } from '@/libs/microcms';
 
 export default async function Home() {
-  // const allLists =await getAllLists();
+const allLists =await getAllLists();
 
   return (
     <div className="container mx-auto px-4 py-8">
