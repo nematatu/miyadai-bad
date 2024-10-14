9ed276fa7bf92c833b62e9a22ef02bff31fb8eac
nematatu <nematatu1003@gmail.com>
2024-10-14 18:56:35 +0900

    update:img→Image

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -10,7 +10,7 @@ export default async function StaticPage() {
     const response=await fetch('https://miyadai-bad.vercel.app/api/works',{
         next: { revalidate: 100 },
     })
-  const { contents } = await getList(domain);
+  const { contents } = await response.json();
 
   if (!contents || contents.length === 0) {
     return <h1>No contents</h1>;
