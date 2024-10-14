c46011b9cb5413550cbc2c9c9166b549ab103b8d
nematatu <nematatu1003@gmail.com>
2024-10-14 18:08:10 +0900

    update:img→Image

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,31 +1,10 @@
 import Link from "next/link";
+import { getList } from "@/libs/microcms";
+import { createClient } from "microcms-js-sdk";
 
-// マイクロCMSからデータを取得する関数
-const getList = async (domain: string) => {
-  const endpoint = `https://miyadai-bad.microcms.io/api/v1/${domain}`;
-  const response = await fetch(endpoint, {
-    method: 'GET',
-    headers: {
-      'X-MICROCMS-API-KEY': process.env.MICROCMS_API_KEY || '',
-      'Content-Type': 'application/json',
-    },
-    cache: 'no-store',
-  });
-
-  if (!response.ok) {
-    const errorData = await response.text(); // エラー詳細を取得
-    console.error(`Error fetching data: ${response.status} ${errorData}`);
-    throw new Error(`Failed to fetch data: ${response.status}`);
-  }
-
-  const data = await response.json();
-  return data;
-};
-
-// ページコンポーネント
+const domain = 'results'
 export default async function StaticPage() {
-  const domain = 'results';
-  const { contents } = await getList(domain); // データを取得
+  const { contents } = await getList(domain);
 
   if (!contents || contents.length === 0) {
     return <h1>No contents</h1>;
@@ -34,9 +13,9 @@ export default async function StaticPage() {
   return (
     <div>
       <ul>
-        {contents.map((post: any) => {
+        {contents.map((post:any) => {
           return (
-            <li className="flex justify-center" key={post.id}>
+            <li className="flex jusitify-center" key={post.id}>
               <Link href={`/${domain}/${post.id}`}>{post.title}</Link>
               <p className="ml-3">{post.publishedAt.split('T')[0]}</p>
             </li>
@@ -46,3 +25,11 @@ export default async function StaticPage() {
     </div>
   );
 }
+
+// export default async function StaticPage() {    
+//     return (
+//         <div>
+//             <h1>Static Page</h1>
+//         </div>
+//     )
+// }
