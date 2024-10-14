d3b20d37f5c672d0b5a563e225a219a3e8728c68
nematatu <nematatu1003@gmail.com>
2024-10-14 16:43:21 +0900

    update:img→Image

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from "next/link";
 import { getList } from "@/libs/microcms";
 import { createClient } from "microcms-js-sdk";
@@ -6,7 +7,6 @@ const domain = 'results'
 export default async function StaticPage() {
   const { contents } = await getList(domain);
   // ページの生成された時間を取得
-  const time = new Date().toLocaleString();
 
   if (!contents || contents.length === 0) {
     return <h1>No contents</h1>;
